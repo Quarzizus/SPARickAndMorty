9f7edca3bc6493c6685c3ce5125826dc1f00f81b
Zizu <angelzizu06@gmail.com>
2021-02-19 14:23:36 -0500

    ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,54 @@
 // acceder en remoto o local a las carpetas
-import { resolve as _resolve } from 'path'
+const path = require('path')
 // trabajar con archivos html
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 //trabajar con archivos css
-import CopyWebpackPlugin from 'copy-webpack-plugin'
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // modulo con toda la configuracion
-export const entry = './src/index.js'
-export const output = {
-    path: _resolve(__dirname, 'dist'),
-    // nombre del archivo
-    filename: 'main.js'
-}
-export const resolve = {
-    // extensiones que vamos a trabajar
-    extensions: ['.js']
-}
-export const module = {
-    rules: [
-        {
-            // identificar archivos que están en nuestro entorno
-            test: /\,js?$/,
-            // gitignore
-            exclude: /node_modules/,
-            use: {
-                // utilizar el loader como preestablecido
-                loader: 'babel-loader'
+module.exports = {
+    // punto de entrada
+    entry: './src/index.js',
+    // donde se aloja
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        // nombre del archivo
+        filename: 'main.js'
+    },
+    resolve: {
+        // extensiones que vamos a trabajar
+        extensions: ['.js']
+    },
+    // donde irán las reglas 
+    module: {
+        rules: [
+            {
+                // identificar archivos que están en nuestro entorno
+                test: /\,js?$/,
+                // gitignore
+                exclude: /node_modules/,
+                use: {
+                    // utilizar el loader como preestablecido
+                    loader: 'babel-loader'
+                }
             }
-        }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // como injectar valores a html
+            inject: true,
+            // donde está el template main
+            template: './public/index.html',
+            // nombre del archivo
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin({
+                patterns: [{
+                // donde está
+                from: './src/styles/styles.css',
+                // a donde va 
+                to: ' ' }],
+        })
     ]
 }
-export const plugins = [
-    new HtmlWebpackPlugin({
-        // como injectar valores a html
-        inject: true,
-        // donde está el template main
-        template: './public/index.html',
-        // nombre del archivo
-        filename: './index.html'
-    }),
-    new CopyWebpackPlugin({
-        patterns: [{
-            // donde está
-            from: './src/styles/styles.css',
-            // a donde va 
-            to: ' '
-        }],
-    })
-]
